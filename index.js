b6911eae7b22d6ddddc29fcb80748b91c431e936
River <nastya.rudyk@gmail.com>
2020-05-28 13:23:09 +0300

    added sessions

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const express = require('express')
-    const bodyParser = require('body-parser')
-   const db = require('./queriesMongoDB')
-    const app = express()
-    const port = 3000
+const bodyParser = require('body-parser')
+const db = require('./queriesMongoDB')
+const app = express()
+const port = 3000
 
-    app.use(bodyParser.json())
-    app.use(
-        bodyParser.urlencoded({
-            extended: true,
-        }))
-    app.get('/', (request, response) => {
-        response.json({
-            hello_world: 'Witness the server!'
-        })
-    })
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    }))
+// app.get('/', (request, response) => {
+//
+//     response.json({
+//         hello_world: 'Witness the server!'
+//     })
+// })
 
-    app.listen(port, () => {
-        console.log(`App running on port ${port}.`)
-    })
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
 
-    app.get('/players', db.getPlayers)
-    app.get('/player/:id', db.getPlayer)
-    app.post('/players', db.createPlayer)
+app.get('/', db.createSessionStart)
+app.get('/players', db.getPlayers)
+app.get('/player/:id', db.getPlayer)
+app.post('/players', db.createPlayer)
+app.post('/startSession', db.createSessionStart)
+app.post('/endSession', db.updateSessionEnd)
--- queriesMongoDB.js
+++ queriesMongoDB.js
@@ -18,8 +18,11 @@ const createPlayer = (request, response) => {
         if (err) throw err;
         var dbo = db.db("mydb");
         var myobj = {name: "John", id: "42"};
-        dbo.collection("players").insertOne(myobj, function (err, res) {
-            if (err) { response.status(500, err); throw err; }
+        dbo.collection("players").insertOne(request.body, function (err, res) {
+            if (err) {
+                response.status(500, err);
+                throw err;
+            }
             console.log("1 document inserted" + res);
             response.status(200).json("1 document inserted" + res);
             db.close();
@@ -27,12 +30,63 @@ const createPlayer = (request, response) => {
     });
 }
 
-const getPlayers = (request, response) => {
-    MongoClient.connect(url, function(err, db) {
+const createSessionStart = (request, response) => {
+    MongoClient.connect(url, function (err, db) {
         if (err) throw err;
         var dbo = db.db("mydb");
-        dbo.collection("players").find({}).toArray(function(err, result) {
-            if (err) { response.status(500, err); throw err; }
+        console.log('>>> ' + Date.now().toString())
+        let session = {playerid: request.param('playerid'), sessionstart: Date.now().toString()};
+
+        dbo.collection("sessions").insertOne(session, function (err, res) {
+            if (err) {
+                response.status(500, err);
+                throw err;
+            }
+            console.log("1 session document inserted" + res.ops[0]._id);
+            response.status(200).json(res.ops[0]._id);
+            db.close();
+        });
+    });
+}
+
+const updateSessionEnd = (request, response) => {
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        const dbo = db.db("mydb");
+        console.log('>>>> request >  ' + request.param);
+
+        // let theQuery = {playerid: request.param('playerid')};
+        let theQuery = {};
+        // let theQuery = {playerid: request.param('playerid'), _id: request.param('sessionid')};
+        // let newValues = {$set: {sessionend: new Date(), score: request.param('score')}};
+        let newValues = {$set: {sessionend: new Date(), score: "10"}};
+
+       // console.log('>>>> theQuery >  ' + request.param('playerid') + ' > ' + request.param('sessionid'));
+        console.log('>>>> newValues >  ' + newValues.score);
+        dbo.collection("sessions").find(theQuery, function (err, res) {
+            console.log("1 session document found" + res.length);
+        })
+        dbo.collection("sessions").updateOne(theQuery, newValues, function (err, res) {
+            if (err) {
+                response.status(500, err);
+                throw err;
+            }
+            console.log("1 session document updated" + res);
+            response.status(200).json("1 document updated" + res);
+            db.close();
+        });
+    });
+}
+
+const getPlayers = (request, response) => {
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        dbo.collection("players").find({}).toArray(function (err, result) {
+            if (err) {
+                response.status(500, err);
+                throw err;
+            }
             console.log(result);
             db.close();
             response.status(200).json(result);
@@ -42,12 +96,15 @@ const getPlayers = (request, response) => {
 }
 
 const getPlayer = (request, response) => {
-    MongoClient.connect(url, function(err, db) {
+    MongoClient.connect(url, function (err, db) {
         if (err) throw err;
         var dbo = db.db("mydb");
         console.log('>>> request id > ' + request.param('id'));
-        dbo.collection("players").find({id : request.param('id')}).toArray(function(err, result) {
-            if (err) { response.status(500, err); throw err; }
+        dbo.collection("players").find({id: request.param('id')}).toArray(function (err, result) {
+            if (err) {
+                response.status(500, err);
+                throw err;
+            }
             console.log(result);
             db.close();
             response.status(200).json(result);
@@ -58,5 +115,7 @@ const getPlayer = (request, response) => {
 module.exports = {
     getPlayers,
     getPlayer,
-    createPlayer
+    createPlayer,
+    createSessionStart,
+    updateSessionEnd
 }
